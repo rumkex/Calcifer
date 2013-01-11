6962a8af8fc21aeb174743e8ffd8564c70efdcba
ambient92 <nikita.tereshin@gmail.com>
2013-01-12 02:50:39 +0400

    Implemented platforms/doors

---
+++ Engine/Physics/TargetConstraint.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Calcifer.Utilities.Logging;
+using Jitter;
+using Jitter.Dynamics;
+using Jitter.Dynamics.Constraints;
+using Jitter.LinearMath;
+
+namespace Calcifer.Engine.Physics
+{
+    public class TargetConstraint: Constraint
+    {
+        private JVector targetVelocity = JVector.Zero;
+
+        public TargetConstraint(RigidBody body)
+            : base(body, null)
+        {}
+
+        public float Velocity { get; set; }
+        public JVector Target { get; set; }
+
+        public override void PrepareForIteration(float timestep)
+        {
+            var delta = Target - Body1.Position;
+            var length = delta.Length();
+            if (length > 1f*Velocity)
+            {
+                if (Velocity > targetVelocity.Length())
+                    targetVelocity = JVector.Normalize(delta)*(targetVelocity.Length() + Velocity*timestep);
+                else
+                    targetVelocity = JVector.Normalize(delta)*Velocity;
+            }
+            else
+                targetVelocity = JVector.Normalize(delta) * (float) Math.Sqrt(length);
+        }
+
+        public override void Iterate()
+        {
+            var deltaVelocity = targetVelocity - Body1.LinearVelocity;
+            if (deltaVelocity.LengthSquared() > 0.000001f)
+                Body1.ApplyImpulse(deltaVelocity*Body1.Mass);
+        }
+    }
+}
--- Engine/Physics/WaypointMovableComponent.cs
+++ Engine/Physics/WaypointMovableComponent.cs
@@ -8,47 +8,37 @@ using OpenTK;
 
 namespace Calcifer.Engine.Physics
 {
-    public class WaypointMovableComponent: DependencyComponent, IUpdateable
+    public class WaypointMovableComponent: DependencyComponent
     {
-        [RequireComponent] private TransformComponent transform;
         [RequireComponent] private PhysicsComponent physics;
         [RequireComponent] private WaypointComponent nodes;
 
-        public WaypointMovableComponent()
-        {
-            Speed = 1.0f;
-        }
-
+        private TargetConstraint constraint;
+        
         protected override void OnAdded(ComponentStateEventArgs registrationArgs)
         {
             base.OnAdded(registrationArgs);
-            physics.Body.IsStatic = true;
             physics.Body.Material.Restitution = 0f;
             physics.Body.Material.KineticFriction = 0.5f;
             physics.Body.Material.StaticFriction = 0.5f;
+            physics.Body.Mass = 1000f;
+            physics.Body.SetMassProperties(JMatrix.Zero, 1/physics.Body.Mass, true);
+            constraint = new TargetConstraint(physics.Body) {Velocity = 3.0f};
+            Activate();
+            physics.Synchronized += (sender, args) =>
+                                        {
+                                            if (!IsOutOfSync)
+                                                physics.World.AddConstraint(constraint);
+                                            else
+                                                physics.World.RemoveConstraint(constraint);
+                                        };
         }
-
-        public void Update(double dt)
-        {
-            if (!Active) return;
-            var target = nodes.Nodes[nodes.CurrentNode];
-            var otherTransform = target.GetComponent<TransformComponent>();
-            var delta = otherTransform.Translation.ToJVector() - physics.Body.Position;
-            if (delta.Length() < dt*Speed)
-            {
-                physics.Body.Position = otherTransform.Translation.ToJVector();
-                Active = false;
-            }
-            else
-                physics.Body.Position += JVector.Normalize(delta) * (float)dt * Speed;
-        }
-
-        public float Speed { get; set; }
-        public bool Active { get; private set; }
-
+        
         public void Activate()
         {
-            Active = true;
+            var target = nodes.Nodes[nodes.CurrentNode];
+            var otherTransform = target.GetComponent<TransformComponent>();
+            constraint.Target = otherTransform.Translation.ToJVector();
         }
     }
 }
